20c3c2a348a34e8ff53e518c92d9b40ae1631332
Atul Anand <atulcleaver@gmail.com>
2022-12-04 11:02:51 +0530

    Added Navbar + Background Design

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './css/Main.css';
+import Navbar from './Components/Navbar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      {/* 
+      - Items Page
+        - Navbar
+        - Heading
+        - Search Bar
+        - ItemList
+        - Footer
+      */}
+        <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "../css/Main.css";
+import "../css/Navbar.css";
+import logo from "../img/logo.png";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a
+            className="navbar-brand"
+            target="_blank"
+            rel="noreferrer"
+            href="https://recycle-it.typedream.app/"
+          >
+            <img
+              src={logo}
+              alt="Logo"
+              className="d-inline-block align-text-top navbar-logo"
+            />
+            Recycle It
+          </a>
+
+          <div className="d-flex">
+            <ul className="nav-list">
+              <li className="nav-list-item">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://twitter.com/AtulACleaver"
+                  className="nav-list-item-link"
+                >
+                  Twitter
+                </a>
+              </li>
+              <li className="nav-list-item">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://recycle-it.typedream.app/#about-me"
+                  className="nav-list-item-link"
+                >
+                  About Me
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
